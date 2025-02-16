789a3388a07934e0b38815921bde23ffb066829d
bas081 <bas080@hotmail.com>
2025-02-16 17:26:03 +0200

    Add edit on github link

---
+++ _data/site.mjs
@@ -0,0 +1,5 @@
+export default {
+  githubRepo: "https://github.com/Promitheas-Badminton-Club/promitheas-badminton-club.github.io/",
+  branch: "main", // Change to your default branch
+  contentDir: "", // Adjust based on where your files are stored in the repo
+};
